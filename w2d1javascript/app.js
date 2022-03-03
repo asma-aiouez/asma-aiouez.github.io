181c7b11a419bda617cd245bfb03a3c10b593a14
asma-aiouez <asma.aiouez@gmail.com>
2022-03-03 07:37:05 -0600

    updating Q 14.b

--- w2d1javascript/app.js
+++ w2d1javascript/app.js
@@ -174,18 +174,17 @@ btn.addEventListener("click", function(event) {
 // b.
 let addProduct = document.getElementById("addProduct");
 addProduct.addEventListener("click", function(event) {
-    event.preventDefault();
     let productNumber = document.getElementById("productNumber");
     let quantity = document.getElementById("quantity");
     let name = document.getElementById("name");
     let unitPrice = document.getElementById("unitPrice");
-    let select = document.getElementsByTagName("select")
+    let select = document.getElementById("supplier")
     let date = document.getElementById("date");
     alert("Product Number: "+productNumber.value
          +" - Quantity: "+quantity.value
          +" - Name: "+name.value
          +" - Unit Price: "+unitPrice.value
-         +" - Supplier: "+select.options[select.selectedIndex].value
+         +" - Supplier: "+select.options[select.selectedIndex].text
          +" - Date: "+date.value
          );
 })*/
